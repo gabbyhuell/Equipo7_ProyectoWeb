94075bd9036fff1af1c1dd7aabb2cbab6852e4df
gabbyhuell@gmail.com <gabbyhuell@gmail.com>
2022-12-21 01:30:45 -0300

    pulido
    actualización nueva rama

--- ProjectFinal/blog/blog/settings.py
+++ ProjectFinal/blog/blog/settings.py
@@ -75,17 +75,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blog.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# configuración mysql en el archivo settings.py para conectar a la base de datos
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
